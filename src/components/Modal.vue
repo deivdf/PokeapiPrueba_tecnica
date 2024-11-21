34c2ea1cc9c39718ef9db7fd72e71f42ed2c71a1
D3ivdf <dfdavidfernando@gmail.com>
2024-11-21 00:04:04 -0500

    change: se hace pequeño cambion en el modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,7 +3,7 @@
     <div class="bg-slate-300 rounded-lg p-8 w-96">
       <button @click="closeModal" class="top-2 right-2 text-slate-100 hover:text-red-700 bg-slate-900 hover:bg-slate-200 pl-5 py-3 pr-5 rounded-full shadow-xl">X</button>
       <img :src="pokemon?.sprites.front_default" :alt="pokemon?.name" class="w-full h-auto mb-4">
-      <div class="transform hover:-translate-y-1 hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out bg-slate-50 hover:bg-slate-500 hover:text-white shadow-xl font-bold py-2 px-4 rounded">
+      <div class="transform hover:-translate-y-1 hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out bg-slate-50 hover:bg-slate-100 shadow-xl font-bold py-2 px-4 rounded">
           <h2 class="text-2xl font-bold mb-10 text-center">{{ pokemon?.name }}:</h2>
           <p v-if="pokemon" class="font-semibold ml-4">ID: {{ pokemon.id }}</p>
           <p v-if="pokemon" class="font-semibold  ml-4">Tipos: {{ pokemon.types.map(type => type.type.name).join(', ') }}</p>
